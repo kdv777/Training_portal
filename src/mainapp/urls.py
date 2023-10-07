ec3460b236951ff7b8c59ef10e3a5d7df52db960
Posvian <posvianski@mail.ru>
2023-10-07 12:55:55 +0300

    добавил имена в urls, сделал часть переходов

--- src/mainapp/urls.py
+++ src/mainapp/urls.py
@@ -6,8 +6,8 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", views.MainPageView.as_view()),
-    path("contacts/", views.ContactsPageView.as_view()),
-    path("catalog/", views.CatalogPageView.as_view()),
-    path("login/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="index"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("catalog/", views.CatalogPageView.as_view(), name="catalog"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
