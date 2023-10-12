0ed31a126aa1277846afa8100b20845282288cf4
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-10-13 01:41:58 +0200

    Добавила регистрацию, вход и выход.

--- src/authapp/urls.py
+++ src/authapp/urls.py
@@ -8,4 +8,5 @@ app_name = AuthappConfig.name
 urlpatterns = [
     path("login/", views.LoginPageView.as_view(), name="login"),
     path("register/", views.RegisterPageView.as_view(), name="register"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
 ]
--- src/authapp/views.py
+++ src/authapp/views.py
@@ -1,9 +1,54 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import TemplateView
+
+from authapp.models import User
 
 
 class LoginPageView(TemplateView):
     template_name = "authapp/login.html"
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("mainapp:index")
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("mainapp:index")
+        user = authenticate(
+            username=request.POST.get("username"), password=request.POST.get("password")
+        )
+        if user is not None:
+            login(request, user)
+            return redirect("mainapp:index")
+        return redirect("authapp:login")
+
 
 class RegisterPageView(TemplateView):
     template_name = "authapp/register.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("mainapp:index")
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        password_confirm = request.POST.get("password_confirm")
+        if not all([username, password, password_confirm]):
+            return redirect("authapp:register")
+        if password != password_confirm:
+            return redirect("authapp:register")
+        user = User(username=username)
+        user.set_password(password)
+        user.save()
+        return redirect("authapp:login")
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("mainapp:index")
