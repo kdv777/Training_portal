1367c77f9f1c3b88af91d8e998bc061a4d23b20b
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-10-14 13:31:07 +0200

    Починила логин

--- src/authapp/views.py
+++ src/authapp/views.py
@@ -1,31 +1,23 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, View
 
 from authapp.models import User
 
 
-class LoginPageView(TemplateView):
+class LoginPageView(LoginView):
     template_name = "mainapp/login.html"
+    redirect_authentacated_user = True
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect("mainapp:index")
-        return super().get(request, *args, **kwargs)
+    def get_success_url(self):
+        return reverse_lazy("mainapp:cabinet")
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect("mainapp:index")
-        user = authenticate(
-            username=request.POST.get("username"), password=request.POST.get("password")
-        )
-        if user is not None:
-            login(request, user)
-            return redirect("mainapp:index")
-        else:
-            messages.error(self.request, "Invalid username or password")
-        return redirect("authapp:login")
+    def form_invalid(self, form):
+        messages.error(self.request, "Invalid username or password")
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class RegisterPageView(TemplateView):
