e85c1e9b278d2be69f709e31529d59fc56f21378
ecrvmal <ecrvmal@yandex.ru>
2023-10-14 22:16:32 +0300

    vmal_sprint4

--- src/mainapp/admin.py
+++ src/mainapp/admin.py
@@ -37,3 +37,7 @@ class ArticleAdmin(admin.ModelAdmin):
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
