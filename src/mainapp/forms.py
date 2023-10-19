4ad1e9b991d4b50ddbe5de0b03107a07bf0f1e2e
ecrvmal <ecrvmal@yandex.ru>
2023-10-19 15:10:37 +0300

    vmal course_create draft1

--- src/mainapp/forms.py
+++ src/mainapp/forms.py
@@ -4,17 +4,17 @@ from django import forms
 from mainapp.models import Course, Lesson, Post
 
 # ref: https://stackoverflow.com/questions/2374224/django-working-with-multiple-forms/2374240#2374240
-
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'form-control'
-                field.help_text = ''
+#
+# class CourseForm(forms.ModelForm):
+#     class Meta:
+#         model = Course
+#         fields = '__all__'
+#
+#         def __init__(self, *args, **kwargs):
+#             super().__init__(*args, **kwargs)
+#             for field_name, field in self.fields.items():
+#                 field.widget.attrs['class'] = 'form-control'
+#                 field.help_text = ''
 
 
 class LessonForm(forms.ModelForm):
