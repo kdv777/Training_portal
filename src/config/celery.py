79949adf1607a75908b499023b3d082db9ee538e
Anastasia <aastavitskaya@gmail.com>
2023-10-30 00:38:41 +0400

    new_containers

--- src/config/celery.py
+++ src/config/celery.py
@@ -1,9 +1,4 @@
-import os
-
 from celery import Celery
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 celery_app = Celery("config")
 celery_app.config_from_object("django.conf:settings", namespace="CELERY")
