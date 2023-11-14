61176fc55040f99726a96f400432bee0f6d45811
Belyakov_Roman <belyakov1981@mail.ru>
2023-11-14 09:42:51 +0300

    Sphinx and locust

--- src/documentation_project/source/conf.py
+++ src/documentation_project/source/conf.py
@@ -7,8 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Training_portal'
-copyright = '2023, Pythons'
-author = 'Pythons'
+copyright = '2023, Belyakov_Roman'
+author = 'Belyakov_Roman'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
@@ -16,6 +16,7 @@ release = '1.0.0'
 
 import os
 import sys
+#from src.config.settings import *
 
 # Указываем путь до root-папки проекта Django
 # Путь относительно файла conf.py
@@ -31,11 +32,11 @@ django_settings = 'config.settings'
 extensions = [
     'sphinxcontrib_django',
     'sphinx.ext.autodoc',
-    'rst2pdf.pdfbuilder'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,11 +45,3 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [
- ('index', 'Training_portal.tex', u'Training_portal', u'Belyakov_Roman', 'manual'),
-]
-
-pdf_documents = [('index', u'Training_portal', u'Training_portal', u'Belyakov_Roman'),]
